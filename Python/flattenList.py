377eb928b54784c6a009dbec18c720df885c634e
AnilALADAG <73140552+AnilALADAG@users.noreply.github.com>
2022-06-23 05:12:01 +0300

    Create flattenList.py

---
+++ Python/flattenList.py
@@ -0,0 +1,19 @@
+flattedList = []
+inputList = [[1,'a',['cat'],2],[[[3]],'dog'],4,5]
+
+def flattenList(l):
+    for element in l:
+        if isinstance(element, list):
+            flattenList(element)
+        else:
+            flattedList.append(element)
+
+flattenList(inputList)
+print(flattedList)
+
+
+
+
+
+
+
